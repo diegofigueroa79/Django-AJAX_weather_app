c7139d234a2bb6f89a6833ce829c1378d581222d
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-06-21 02:19:39 -0700

    modifed home function list all cities from db into context dict

--- django_weather/weather/views.py
+++ django_weather/weather/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 
+from .models import City
+
 # Create your views here.
 def home(request):
-	context = {}
+	cities = City.objects.all()
+	
+	context = {
+		'cities': cities,
+	}
 	
 	return render(request, 'weather/weather.html', context)
