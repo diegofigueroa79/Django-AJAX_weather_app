a2c6457b7815ae93312e7682ad307153f9cbbaab
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-06-22 20:58:08 -0700

    add instantiation of CityForm

--- django_weather/weather/views.py
+++ django_weather/weather/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
 
 from .models import City
+from .forms import CityForm
 
 # Create your views here.
 def home(request):
 	cities = City.objects.all()
 	
+	form = CityForm()
+	
 	context = {
 		'cities': cities,
+		'form': form,
 	}
 	
 	return render(request, 'weather/weather.html', context)
