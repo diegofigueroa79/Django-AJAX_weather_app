dc83645d0106bc465d25db416d7177f2b84a43f4
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-06-26 11:35:57 -0700

    add form functionality to views to accept form data and pull city info from api

--- django_weather/weather/views.py
+++ django_weather/weather/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
+import requests as r
+
 from .models import City
 from .forms import CityForm
 
 # Create your views here.
 def home(request):
 	cities = City.objects.all()
+	url = 'http://api.openweathermap.org/data/2.5/weather?q={}&APPID=ad066ae896d05c232621a2f18019a69b'
+	
+	if request.method == 'POST':
+		form = CityForm(request.POST)
+		if form.is_valid():
+			name = form.cleaned_data['name'].lower()
+			response = r.get(url.format(name)).json()
+			print(response['name'])
 	
 	form = CityForm()
 	
