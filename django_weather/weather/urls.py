481c0f8c82edc7ce9419d85d23b27707056eafbe
diegofigueroa79 <diegofigueroa79@gmail.com>
2020-07-08 01:22:23 -0700

    added 'add/' url pattern

--- django_weather/weather/urls.py
+++ django_weather/weather/urls.py
@@ -6,5 +6,6 @@ from . import views
 app_name = 'weather'
 urlpatterns = [
 	path('', views.home, name="home"),
+	path('add/', views.add, name="add"),
 	path('delete/<city_name>/', views.delete, name="delete"),
 ] 
